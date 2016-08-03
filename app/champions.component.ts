0f6f2bea395d096c60b949a85a3f3e4fde18e3a8
ethan1341 <ethan1341@yahoo.com>
2016-08-02 19:50:41 -0700

    Added gulp for SCSS compilation

--- app/champions.component.ts
+++ app/champions.component.ts
@@ -1,3 +1,8 @@
 import { Component, OnInit } from '@angular/core'; 
 import { Router } from '@angular/router';
 import { Champion } from './champion.ts'
+
+@Component({
+    selector: 'my-heroes',
+    templateUrl
+})
