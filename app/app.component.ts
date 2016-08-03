26ee9efd32beaf953c482aefde058e899aae5b70
ethan1341 <ethan1341@yahoo.com>
2016-08-02 18:43:51 -0700

    directory refactor, component addition

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { LoginComponent } from './login.component';
 @Component({
     selector: 'my-app',
-    templateUrl:'./app/home.html',
+    templateUrl:'app/html/home.html',
     directives:[LoginComponent]
 })
 export class AppComponent { }
--- app/login.component.ts
+++ app/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'login-selector',
-    templateUrl:'./app/login.html'
+    templateUrl:'app/html/login.html'
 })
 
 export class LoginComponent{}
